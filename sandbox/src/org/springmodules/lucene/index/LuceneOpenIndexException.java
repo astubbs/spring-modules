fdea7ac7724ee40e42f99197ea09fe4b21a09513
templth <templth>
2005-04-15 07:24:35 +0000

    Added exceptions for the Lucene indexation support

---
+++ sandbox/src/org/springmodules/lucene/index/LuceneOpenIndexException.java
@@ -0,0 +1,25 @@
+/*
+ * Créé le 11 avr. 05
+ *
+ * Pour changer le modèle de ce fichier généré, allez à :
+ * Fenêtre&gt;Préférences&gt;Java&gt;Génération de code&gt;Code et commentaires
+ */
+package org.springmodules.lucene.index;
+
+import java.io.IOException;
+
+import org.springframework.core.NestedRuntimeException;
+
+/**
+ * @author ttemplier
+ *
+ * Pour changer le modèle de ce commentaire de type généré, allez à :
+ * Fenêtre&gt;Préférences&gt;Java&gt;Génération de code&gt;Code et commentaires
+ */
+public class LuceneOpenIndexException extends NestedRuntimeException {
+
+	public LuceneOpenIndexException(String msg,IOException ex) {
+		super(msg,ex);
+	}
+
+}
