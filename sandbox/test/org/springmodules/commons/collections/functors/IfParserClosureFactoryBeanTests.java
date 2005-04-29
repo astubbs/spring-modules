9f611de6449c7925d255c2bbbdae3668e65fa78f
devijvers <devijvers>
2005-04-29 13:50:56 +0000

    Update unit test.

--- sandbox/test/org/springmodules/commons/collections/functors/IfParserClosureFactoryBeanTests.java
+++ sandbox/test/org/springmodules/commons/collections/functors/IfParserClosureFactoryBeanTests.java
@@ -1,24 +1,13 @@
 package org.springmodules.commons.collections.functors;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import junit.framework.TestCase;
-
-import org.apache.commons.collections.Closure;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springmodules.validation.functions.BeanPropertyFunction;
-import org.springmodules.validation.functions.Function;
 
 /**
  * @author Steven Devijver
  * @since 29-04-2005
  *
  */
-public class IfParserClosureFactoryBeanTests extends TestCase {
+public class IfParserClosureFactoryBeanTests extends AbstractOrderTestCase {
 
-	private static ThreadLocal tl = new ThreadLocal();
 	
 	public IfParserClosureFactoryBeanTests() {
 		super();
@@ -28,27 +17,7 @@ public class IfParserClosureFactoryBeanTests extends TestCase {
 		super(arg0);
 	}
 
-	private Map getOrder(int totalAmount, String qualification) {
-		Map customer = new HashMap();
-		customer.put("qualification", qualification);
-		Map order = new HashMap();
-		order.put("totalAmount", new Integer(totalAmount));
-		order.put("customer", customer);
-		return order;
-	}
-	
-	private String getPriority(Object order) {
-		Function f = new BeanPropertyFunction("priority");
-		return (String)f.getResult(order);
-	}
-	
-	private Closure getClosure() {
-		if (tl.get() == null) { 
-			tl.set(new ClassPathXmlApplicationContext("org/springmodules/commons/collections/functors/if-parser-closure-tests.xml"));
-		}
-		ApplicationContext appCtx = (ApplicationContext)tl.get();
-		return (Closure)appCtx.getBean("applyPriority");
-	}
+	protected String getBeanConfig() { return "org/springmodules/commons/collections/functors/if-parser-closure-tests.xml"; }
 	
 	public void test1() {
 		Object order = getOrder(500, "bronze");
