204582b5da90f7ba338bb65cd7341359728d1a13
alruiz15 <alruiz15>
2005-06-22 08:48:46 +0000

    Issue number: MOD-31 Added to CVS.

---
+++ sandbox/src/org/springmodules/remoting/xmlrpc/XmlRpcMethodNotFoundException.java
@@ -0,0 +1,61 @@
+/* 
+ * Created on Jun 15, 2005
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright @2005 the original author or authors.
+ */
+package org.springmodules.remoting.xmlrpc;
+
+/**
+ * <p>
+ * Exception thrown when a XML-RPC request specifies a method that does not
+ * exist.
+ * </p>
+ * 
+ * @author Alex Ruiz
+ * 
+ * @version $Revision: 1.1 $ $Date: 2005/06/22 08:48:46 $
+ */
+public class XmlRpcMethodNotFoundException extends XmlRpcException {
+
+  /**
+   * Version number of this class.
+   * 
+   * @see java.io.Serializable
+   */
+  private static final long serialVersionUID = 3257005449604510518L;
+
+  /**
+   * Constructor.
+   * 
+   * @param msg
+   *          the detail message.
+   */
+  public XmlRpcMethodNotFoundException(String msg) {
+    super(msg);
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param msg
+   *          the detail message.
+   * @param nestedException
+   *          the nested exception.
+   */
+  public XmlRpcMethodNotFoundException(String msg, Throwable nestedException) {
+    super(msg, nestedException);
+  }
+
+}
