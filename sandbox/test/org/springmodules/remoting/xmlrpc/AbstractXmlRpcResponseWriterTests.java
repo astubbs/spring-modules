21fd47326f3864bea86ba729b45722bfa226927b
alruiz15 <alruiz15>
2005-06-26 17:20:17 +0000

    Issue number:  MOD-31 Added more test cases.

--- sandbox/test/org/springmodules/remoting/xmlrpc/AbstractXmlRpcResponseWriterTests.java
+++ sandbox/test/org/springmodules/remoting/xmlrpc/AbstractXmlRpcResponseWriterTests.java
@@ -17,6 +17,12 @@
  */
 package org.springmodules.remoting.xmlrpc;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import junit.framework.TestCase;
 
 import org.apache.commons.codec.binary.Base64;
@@ -24,8 +30,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springmodules.remoting.xmlrpc.support.XmlRpcBase64;
 import org.springmodules.remoting.xmlrpc.support.XmlRpcBoolean;
+import org.springmodules.remoting.xmlrpc.support.XmlRpcDateTime;
+import org.springmodules.remoting.xmlrpc.support.XmlRpcDouble;
 import org.springmodules.remoting.xmlrpc.support.XmlRpcElement;
+import org.springmodules.remoting.xmlrpc.support.XmlRpcInteger;
 import org.springmodules.remoting.xmlrpc.support.XmlRpcResponse;
+import org.springmodules.remoting.xmlrpc.support.XmlRpcString;
 
 /**
  * <p>
@@ -35,7 +45,7 @@ import org.springmodules.remoting.xmlrpc.support.XmlRpcResponse;
  * 
  * @author Alex Ruiz
  * 
- * @version $Revision: 1.1 $ $Date: 2005/06/24 11:59:54 $
+ * @version $Revision: 1.2 $ $Date: 2005/06/26 17:20:17 $
  */
 public abstract class AbstractXmlRpcResponseWriterTests extends TestCase {
 
@@ -109,6 +119,8 @@ public abstract class AbstractXmlRpcResponseWriterTests extends TestCase {
 
     byte[] actualResponse = this.writer.writeResponse(response);
     String actual = new String(actualResponse, 0, actualResponse.length);
+
+    this.logger.debug(response);
     this.logger.debug("XML-RPC response: " + actual);
 
     assertEquals("<XML-RPC response>", expected, actual);
@@ -139,6 +151,113 @@ public abstract class AbstractXmlRpcResponseWriterTests extends TestCase {
 
     byte[] actualResponse = this.writer.writeResponse(response);
     String actual = new String(actualResponse, 0, actualResponse.length);
+
+    this.logger.debug(response);
+    this.logger.debug("XML-RPC response: " + actual);
+
+    assertEquals("<XML-RPC response>", expected, actual);
+  }
+
+  public final void testWriteResponseWithDateTimeParameters() {
+    DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd'T'HH:mm:ss");
+
+    Calendar calendar = new GregorianCalendar();
+    calendar.set(Calendar.MILLISECOND, 0);
+
+    Date value = calendar.getTime();
+    XmlRpcDateTime xmlRpcDateTime = new XmlRpcDateTime(value);
+    XmlRpcElement[] parameters = { xmlRpcDateTime };
+
+    XmlRpcResponse response = new XmlRpcResponse(parameters);
+
+    StringBuffer builder = new StringBuffer(256);
+    builder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+    builder.append("<methodResponse><params><param><value><dateTime.iso8601>");
+    builder.append(dateFormat.format(value));
+    builder
+        .append("</dateTime.iso8601></value></param></params></methodResponse>");
+
+    String expected = builder.toString();
+
+    byte[] actualResponse = this.writer.writeResponse(response);
+    String actual = new String(actualResponse, 0, actualResponse.length);
+
+    this.logger.debug(response);
+    this.logger.debug("XML-RPC response: " + actual);
+
+    assertEquals("<XML-RPC response>", expected, actual);
+  }
+
+  public final void testWriteResponseWithDoubleParameters() {
+    Double value = new Double(782.99);
+
+    XmlRpcDouble xmlRpcDouble = new XmlRpcDouble(value);
+    XmlRpcElement[] parameters = { xmlRpcDouble };
+
+    XmlRpcResponse response = new XmlRpcResponse(parameters);
+
+    StringBuffer builder = new StringBuffer(256);
+    builder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+    builder.append("<methodResponse><params><param><value><double>");
+    builder.append(value);
+    builder.append("</double></value></param></params></methodResponse>");
+
+    String expected = builder.toString();
+
+    byte[] actualResponse = this.writer.writeResponse(response);
+    String actual = new String(actualResponse, 0, actualResponse.length);
+
+    this.logger.debug(response);
+    this.logger.debug("XML-RPC response: " + actual);
+
+    assertEquals("<XML-RPC response>", expected, actual);
+  }
+
+  public final void testWriteResponseWithIntegerParameters() {
+    Integer value = new Integer(54);
+
+    XmlRpcInteger xmlRpcInteger = new XmlRpcInteger(value);
+    XmlRpcElement[] parameters = { xmlRpcInteger };
+
+    XmlRpcResponse response = new XmlRpcResponse(parameters);
+
+    StringBuffer builder = new StringBuffer(256);
+    builder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+    builder.append("<methodResponse><params><param><value><i4>");
+    builder.append(value);
+    builder.append("</i4></value></param></params></methodResponse>");
+
+    String expected = builder.toString();
+
+    byte[] actualResponse = this.writer.writeResponse(response);
+    String actual = new String(actualResponse, 0, actualResponse.length);
+
+    this.logger.debug(response);
+    this.logger.debug("XML-RPC response: " + actual);
+
+    assertEquals("<XML-RPC response>", expected, actual);
+  }
+
+  public final void testWriteResponseWithStringParameters() {
+    String value = "X-Wing";
+
+    XmlRpcString xmlRpcString = new XmlRpcString(value);
+    XmlRpcElement[] parameters = { xmlRpcString };
+
+    XmlRpcResponse response = new XmlRpcResponse(parameters);
+
+    StringBuffer builder = new StringBuffer(256);
+    builder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+    builder.append("<methodResponse><params><param><value><string>");
+    builder.append(value);
+    builder.append("</string></value></param></params></methodResponse>");
+
+    String expected = builder.toString();
+
+    byte[] actualResponse = this.writer.writeResponse(response);
+    String actual = new String(actualResponse, 0, actualResponse.length);
+
+    this.logger.debug(response);
     this.logger.debug("XML-RPC response: " + actual);
 
     assertEquals("<XML-RPC response>", expected, actual);
