a2c5832baadd0d0a014225286ad2ac2563303774
alruiz15 <alruiz15>
2005-06-29 17:24:44 +0000

    Issue number:  MOD-31 Added more test cases.

--- sandbox/test/org/springmodules/remoting/xmlrpc/AbstractXmlRpcResponseWriterTests.java
+++ sandbox/test/org/springmodules/remoting/xmlrpc/AbstractXmlRpcResponseWriterTests.java
@@ -28,6 +28,7 @@ import junit.framework.TestCase;
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springmodules.remoting.xmlrpc.support.XmlRpcArray;
 import org.springmodules.remoting.xmlrpc.support.XmlRpcBase64;
 import org.springmodules.remoting.xmlrpc.support.XmlRpcBoolean;
 import org.springmodules.remoting.xmlrpc.support.XmlRpcDateTime;
@@ -36,6 +37,8 @@ import org.springmodules.remoting.xmlrpc.support.XmlRpcElement;
 import org.springmodules.remoting.xmlrpc.support.XmlRpcInteger;
 import org.springmodules.remoting.xmlrpc.support.XmlRpcResponse;
 import org.springmodules.remoting.xmlrpc.support.XmlRpcString;
+import org.springmodules.remoting.xmlrpc.support.XmlRpcStruct;
+import org.springmodules.remoting.xmlrpc.support.XmlRpcStruct.XmlRpcMember;
 
 /**
  * <p>
@@ -45,7 +48,7 @@ import org.springmodules.remoting.xmlrpc.support.XmlRpcString;
  * 
  * @author Alex Ruiz
  * 
- * @version $Revision: 1.2 $ $Date: 2005/06/26 17:20:17 $
+ * @version $Revision: 1.3 $ $Date: 2005/06/29 17:24:44 $
  */
 public abstract class AbstractXmlRpcResponseWriterTests extends TestCase {
 
@@ -101,6 +104,50 @@ public abstract class AbstractXmlRpcResponseWriterTests extends TestCase {
     this.writer = this.getXmlRpcResponseWriterImplementation();
   }
 
+  /**
+   * Verifies that the method
+   * <code>{@link XmlRpcResponseWriter#writeResponse(XmlRpcResponse)}</code>
+   * creates the XML-RPC response correctly if the response contains array
+   * parameters.
+   */
+  public final void testWriteResponseWithArrayParameters() {
+    XmlRpcString firstValue = new XmlRpcString("Darth Vader");
+    XmlRpcString secondValue = new XmlRpcString("Darth Sidius");
+
+    XmlRpcArray xmlRpcArray = new XmlRpcArray();
+    xmlRpcArray.add(firstValue);
+    xmlRpcArray.add(secondValue);
+
+    XmlRpcElement[] parameters = { xmlRpcArray };
+    XmlRpcResponse response = new XmlRpcResponse(parameters);
+
+    StringBuffer builder = new StringBuffer(256);
+    builder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+    builder
+        .append("<methodResponse><params><param><value><array><data><value><string>");
+    builder.append(firstValue.getValue());
+    builder.append("</string></value><value><string>");
+    builder.append(secondValue.getValue());
+    builder
+        .append("</string></value></data></array></value></param></params></methodResponse>");
+
+    String expected = builder.toString();
+
+    byte[] actualResponse = this.writer.writeResponse(response);
+    String actual = new String(actualResponse, 0, actualResponse.length);
+
+    this.logger.debug(response);
+    this.logger.debug("XML-RPC response: " + actual);
+
+    assertEquals("<XML-RPC response>", expected, actual);
+  }
+
+  /**
+   * Verifies that the method
+   * <code>{@link XmlRpcResponseWriter#writeResponse(XmlRpcResponse)}</code>
+   * creates the XML-RPC response correctly if the response contains base64
+   * parameters.
+   */
   public final void testWriteResponseWithBase64Parameters() {
     byte[] value = { 4, 6, 99, 5 };
     byte[] encodedValue = Base64.encodeBase64(value);
@@ -126,6 +173,12 @@ public abstract class AbstractXmlRpcResponseWriterTests extends TestCase {
     assertEquals("<XML-RPC response>", expected, actual);
   }
 
+  /**
+   * Verifies that the method
+   * <code>{@link XmlRpcResponseWriter#writeResponse(XmlRpcResponse)}</code>
+   * creates the XML-RPC response correctly if the response contains boolean
+   * parameters.
+   */
   public final void testWriteResponseWithBooleanParameters() {
     XmlRpcBoolean[] parameters = { new XmlRpcBoolean(Boolean.TRUE),
         new XmlRpcBoolean(Boolean.FALSE) };
@@ -158,6 +211,12 @@ public abstract class AbstractXmlRpcResponseWriterTests extends TestCase {
     assertEquals("<XML-RPC response>", expected, actual);
   }
 
+  /**
+   * Verifies that the method
+   * <code>{@link XmlRpcResponseWriter#writeResponse(XmlRpcResponse)}</code>
+   * creates the XML-RPC response correctly if the response contains date/time
+   * parameters.
+   */
   public final void testWriteResponseWithDateTimeParameters() {
     DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd'T'HH:mm:ss");
 
@@ -188,6 +247,12 @@ public abstract class AbstractXmlRpcResponseWriterTests extends TestCase {
     assertEquals("<XML-RPC response>", expected, actual);
   }
 
+  /**
+   * Verifies that the method
+   * <code>{@link XmlRpcResponseWriter#writeResponse(XmlRpcResponse)}</code>
+   * creates the XML-RPC response correctly if the response contains
+   * double-precision signed floating point numbers as parameters.
+   */
   public final void testWriteResponseWithDoubleParameters() {
     Double value = new Double(782.99);
 
@@ -213,6 +278,12 @@ public abstract class AbstractXmlRpcResponseWriterTests extends TestCase {
     assertEquals("<XML-RPC response>", expected, actual);
   }
 
+  /**
+   * Verifies that the method
+   * <code>{@link XmlRpcResponseWriter#writeResponse(XmlRpcResponse)}</code>
+   * creates the XML-RPC response correctly if the response contains four-byte
+   * signed integer parameters.
+   */
   public final void testWriteResponseWithIntegerParameters() {
     Integer value = new Integer(54);
 
@@ -238,6 +309,12 @@ public abstract class AbstractXmlRpcResponseWriterTests extends TestCase {
     assertEquals("<XML-RPC response>", expected, actual);
   }
 
+  /**
+   * Verifies that the method
+   * <code>{@link XmlRpcResponseWriter#writeResponse(XmlRpcResponse)}</code>
+   * creates the XML-RPC response correctly if the response contains string
+   * parameters.
+   */
   public final void testWriteResponseWithStringParameters() {
     String value = "X-Wing";
 
@@ -251,6 +328,50 @@ public abstract class AbstractXmlRpcResponseWriterTests extends TestCase {
     builder.append("<methodResponse><params><param><value><string>");
     builder.append(value);
     builder.append("</string></value></param></params></methodResponse>");
+
+    String expected = builder.toString();
+
+    byte[] actualResponse = this.writer.writeResponse(response);
+    String actual = new String(actualResponse, 0, actualResponse.length);
+
+    this.logger.debug(response);
+    this.logger.debug("XML-RPC response: " + actual);
+
+    assertEquals("<XML-RPC response>", expected, actual);
+  }
+
+  /**
+   * Verifies that the method
+   * <code>{@link XmlRpcResponseWriter#writeResponse(XmlRpcResponse)}</code>
+   * creates the XML-RPC response correctly if the response contains struct
+   * parameters.
+   */
+  public final void testWriteResponseWithStructParameters() {
+    XmlRpcMember integerMember = new XmlRpcMember("id", new XmlRpcInteger(
+        new Integer(56)));
+    XmlRpcMember stringMember = new XmlRpcMember("fullName", new XmlRpcString(
+        "Luke Skywalker"));
+
+    XmlRpcStruct xmlRpcStruct = new XmlRpcStruct();
+    xmlRpcStruct.add(integerMember);
+    xmlRpcStruct.add(stringMember);
+
+    XmlRpcElement[] parameters = { xmlRpcStruct };
+    XmlRpcResponse response = new XmlRpcResponse(parameters);
+
+    StringBuffer builder = new StringBuffer(256);
+    builder.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+    builder
+        .append("<methodResponse><params><param><value><struct><member><name>");
+    builder.append(integerMember.name);
+    builder.append("</name><value><i4>");
+    builder.append(((XmlRpcInteger) integerMember.value).getValue());
+    builder.append("</i4></value></member><member><name>");
+    builder.append(stringMember.name);
+    builder.append("</name><value><string>");
+    builder.append(((XmlRpcString) stringMember.value).getValue());
+    builder
+        .append("</string></value></member></struct></value></param></params></methodResponse>");
 
     String expected = builder.toString();
 
