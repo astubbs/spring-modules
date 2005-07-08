f493e7c6e4de0b435eaef1a57d018602e7f62814
templth <templth>
2005-07-08 14:21:08 +0000

    Updated database and sql scripts

--- samples/lucene/db/data.sql
+++ samples/lucene/db/data.sql
@@ -1,2 +1,6 @@
 insert into BOOK (BOOK_ID,BOOK_NAME) values(1,'My book');
 insert into BOOK_PAGE (BOOK_PAGE_ID,BOOK_ID,BOOK_PAGE_TEXT) values(1,1,'Here is a test to indexing a database with lucene and spring');
+
+insert into CATEGORY(CATEGORY_ID,CATEGORY_NAME) values(1,'Category 1');
+insert into CATEGORY(CATEGORY_ID,CATEGORY_NAME) values(2,'Category 2');
+insert into CATEGORY(CATEGORY_ID,CATEGORY_NAME) values(3,'Category 3');
--- samples/lucene/db/hsql-schema.sql
+++ samples/lucene/db/hsql-schema.sql
@@ -12,3 +12,16 @@ create table BOOK_PAGE
     BOOK_PAGE_TEXT VARCHAR,
     constraint PK_BOOK_PAGE primary key (BOOK_PAGE_ID,BOOK_ID)
 );
+
+create table CATEGORY
+(
+    CATEGORY_ID    INT not null,
+    CATEGORY_NAME  VARCHAR,
+    constraint PK_CATEGORY primary key (CATEGORY_ID)
+);
+
+create table DOCUMENT_ID
+(
+    NEXT_DOCUMENT_ID INT not null,
+    constraint PK_DOCUMENT_ID primary key (NEXT_DOCUMENT_ID)
+);
