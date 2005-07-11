083f8908b6613e9b3e224a96ebf3d745d480d5a9
templth <templth>
2005-07-11 15:55:09 +0000

    Updated script to initialize datas for document id

--- samples/lucene/db/data.sql
+++ samples/lucene/db/data.sql
@@ -1,6 +1,8 @@
 insert into BOOK (BOOK_ID,BOOK_NAME) values(1,'My book');
 insert into BOOK_PAGE (BOOK_PAGE_ID,BOOK_ID,BOOK_PAGE_TEXT) values(1,1,'Here is a test to indexing a database with lucene and spring');
 
-insert into CATEGORY(CATEGORY_ID,CATEGORY_NAME) values(1,'Category 1');
-insert into CATEGORY(CATEGORY_ID,CATEGORY_NAME) values(2,'Category 2');
-insert into CATEGORY(CATEGORY_ID,CATEGORY_NAME) values(3,'Category 3');
+insert into CATEGORY (CATEGORY_ID,CATEGORY_NAME) values(1,'Category 1');
+insert into CATEGORY (CATEGORY_ID,CATEGORY_NAME) values(2,'Category 2');
+insert into CATEGORY (CATEGORY_ID,CATEGORY_NAME) values(3,'Category 3');
+
+insert into DOCUMENT_ID (NEXT_DOCUMENT_ID) values(1);
