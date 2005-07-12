5a56fa8dbab4803b7e2e3444c030e9da4323599f
sethladd <sethladd>
2005-07-12 22:02:11 +0000

    fixed  license CVS: ---------------------------------------------------------------------- CVS: Issue number: CVS:   If this change addresses one or more issues, CVS:   then enter the issue number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to the project by someone else; i.e., CVS:   they sent us a patch or a set of diffs, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.

--- src/test/org/springmodules/commons/lang/CommonsEnumPropertyEditorTests.java
+++ src/test/org/springmodules/commons/lang/CommonsEnumPropertyEditorTests.java
@@ -1,13 +1,19 @@
-/*
- * Copyright (c) 2004 Hawaii Information Consortium
- * 220 S. King St #2190, Honolulu, HI 96813
- * All rights reserved.
+/* 
+ * Created on July 12, 2005
  *
- * This software is the confidential and proprietary information
- * of Hawaii Information Consortium. ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with HIC.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright @2005 the original author or authors.
  */
 package org.springmodules.commons.lang;
 
