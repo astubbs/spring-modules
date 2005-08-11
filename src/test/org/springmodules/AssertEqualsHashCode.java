d17921df4f737063a30cd08860933da1fdb4b1f8
alruiz15 <alruiz15>
2005-08-11 04:30:19 +0000

    Added to CVS.

---
+++ src/test/org/springmodules/AssertEqualsHashCode.java
@@ -0,0 +1,74 @@
+/* 
+ * Created on Aug 8, 2005
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright @2005 the original author or authors.
+ */
+package org.springmodules;
+
+import junit.framework.Assert;
+
+/**
+ * <p>
+ * Helper methods for implementations of
+ * <code>{@link EqualsHashCodeTestCase}</code>.
+ * </p>
+ * 
+ * @author Alex Ruiz
+ * 
+ * @version $Revision$ $Date$
+ */
+public class AssertEqualsHashCode {
+
+  /**
+   * @see EqualsHashCodeTestCase#testEqualsHashCodeRelationship()
+   */
+  public static void assertEqualsHashCodeRelationshipIsCorrect(Object first,
+      Object second) {
+    Assert.assertEquals(first, second);
+    Assert.assertEquals(first.hashCode(), second.hashCode());
+  }
+
+  /**
+   * @see EqualsHashCodeTestCase#testEqualsIsReflexive()
+   */
+  public static void assertEqualsIsReflexive(Object obj) {
+    Assert.assertEquals(obj, obj);
+  }
+
+  /**
+   * @see EqualsHashCodeTestCase#testEqualsIsSymmetric()
+   */
+  public static void assertEqualsIsSymmetric(Object first, Object second) {
+    Assert.assertEquals(first, second);
+    Assert.assertEquals(second, first);
+  }
+
+  /**
+   * @see EqualsHashCodeTestCase#testEqualsIsTransitive()
+   */
+  public static void assertEqualsIsTransitive(Object first, Object second,
+      Object third) {
+    Assert.assertEquals(first, second);
+    Assert.assertEquals(second, third);
+    Assert.assertEquals(first, third);
+  }
+
+  /**
+   * @see EqualsHashCodeTestCase#testEqualsNullComparison()
+   */
+  public static void assertEqualsNullComparisonReturnsFalse(Object obj) {
+    Assert.assertFalse(obj.equals(null));
+  }
+}
---
+++ src/test/org/springmodules/cache/provider/CacheProfileValidatorTests.java
@@ -0,0 +1,128 @@
+/* 
+ * Created on Aug 5, 2005
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright @2005 the original author or authors.
+ */
+package org.springmodules.cache.provider;
+
+import junit.framework.TestCase;
+
+/**
+ * <p>
+ * Unit Tests for <code>{@link AbstractCacheProfileValidator}</code>.
+ * </p>
+ * 
+ * @author Alex Ruiz
+ * 
+ * @version $Revision$ $Date$
+ */
+public final class CacheProfileValidatorTests extends TestCase {
+
+  private class MockCacheProfileValidator extends AbstractCacheProfileValidator {
+
+    InvalidCacheProfileException exceptionToThrow;
+
+    Class targetClass;
+
+    /**
+     * @see AbstractCacheProfileValidator#getTargetClass()
+     */
+    protected Class getTargetClass() {
+      return this.targetClass;
+    }
+
+    /**
+     * @see AbstractCacheProfileValidator#validateCacheProfileProperties(java.lang.Object)
+     */
+    protected void validateCacheProfileProperties(Object cacheProfile)
+        throws InvalidCacheProfileException {
+      if (this.exceptionToThrow != null) {
+        // this exception is used to verify that this method was called.
+        throw this.exceptionToThrow;
+      }
+    }
+  }
+
+  /**
+   * Primary object that is under test.
+   */
+  private MockCacheProfileValidator validator;
+
+  public CacheProfileValidatorTests(String name) {
+    super(name);
+  }
+
+  private void assertValidateCacheProfileThrowsInvalidCacheProfileException(
+      Object cacheProfile) {
+    try {
+      this.validator.validateCacheProfile(cacheProfile);
+      fail("Expecting exception <"
+          + InvalidCacheProfileException.class.getName() + ">");
+    } catch (InvalidCacheProfileException exception) {
+      // we are expecting this exception.
+    }
+  }
+
+  protected void setUp() throws Exception {
+    super.setUp();
+
+    this.validator = new MockCacheProfileValidator();
+  }
+
+  /**
+   * Verifies that the method
+   * <code>{@link AbstractCacheProfileValidator#validateCacheProfile(Object)}</code>
+   * validates that the type and the properties of the given cache profile.
+   */
+  public void testValidateCacheProfile() {
+    Object cacheProfile = "A fake cache profile";
+    InvalidCacheProfileException expectedException = new InvalidCacheProfileException(
+        "A fake exception");
+
+    this.validator.targetClass = cacheProfile.getClass();
+    this.validator.exceptionToThrow = expectedException;
+
+    try {
+      this.validator.validateCacheProfile(cacheProfile);
+
+    } catch (InvalidCacheProfileException exception) {
+      assertSame("The properties of the cahe profile were not validated",
+          expectedException, exception);
+    }
+  }
+
+  /**
+   * Verifies that the method
+   * <code>{@link AbstractCacheProfileValidator#validateCacheProfile(Object)}</code>
+   * throws a <code>{@link InvalidCacheProfileException}</code> if the given
+   * cache profile is equal to <code>null</code>.
+   */
+  public void testValidateCacheProfileWithCacheProfileEqualToNull() {
+    this.assertValidateCacheProfileThrowsInvalidCacheProfileException(null);
+  }
+
+  /**
+   * Verifies that the method
+   * <code>{@link AbstractCacheProfileValidator#validateCacheProfile(Object)}</code>
+   * throws a <code>{@link InvalidCacheProfileException}</code> if the class
+   * of the given cache profile is not equal to the target class.
+   */
+  public void testValidateCacheProfileWithCacheProfileHavingClassNotEqualToTargetClass() {
+    Object cacheProfile = "Another fake cache profile";
+    this.validator.targetClass = Integer.class;
+    this
+        .assertValidateCacheProfileThrowsInvalidCacheProfileException(cacheProfile);
+  }
+}
