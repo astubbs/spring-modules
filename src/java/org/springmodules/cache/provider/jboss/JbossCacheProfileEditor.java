0524b2798d406a36c0be038aa90c08a353e0fa8e
alruiz15 <alruiz15>
2005-09-07 03:32:07 +0000

    Changed creation date.

--- src/java/org/springmodules/cache/provider/jboss/JbossCacheProfileEditor.java
+++ src/java/org/springmodules/cache/provider/jboss/JbossCacheProfileEditor.java
@@ -1,5 +1,5 @@
 /* 
- * Created on Jan 19, 2005
+ * Created on Sep 6, 2005
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
