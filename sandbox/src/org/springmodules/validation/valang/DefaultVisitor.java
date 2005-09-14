14fad5bd57adebac3af2400fb8cb3d1df738cc0a
devijvers <devijvers>
2005-09-14 18:36:48 +0000

    Implementation of lifecycle for valang functions.

--- sandbox/src/org/springmodules/validation/valang/DefaultVisitor.java
+++ sandbox/src/org/springmodules/validation/valang/DefaultVisitor.java
@@ -15,9 +15,27 @@
  */ 
 package org.springmodules.validation.valang;
 
+import javax.servlet.ServletContext;
+
 import org.apache.commons.collections.Predicate;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.context.MessageSourceAware;
+import org.springframework.context.ResourceLoaderAware;
 import org.springframework.core.JdkVersion;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.context.ServletContextAware;
 import org.springmodules.util.dateparser.DefaultDateParser;
+import org.springmodules.validation.ValangException;
+import org.springmodules.validation.functions.AbstractFunction;
 import org.springmodules.validation.functions.Function;
 import org.springmodules.validation.functions.LengthOfFunction;
 import org.springmodules.validation.functions.LowerCaseFunction;
@@ -42,10 +60,17 @@ import org.springmodules.validation.predicates.Operator;
  * @author Steven Devijver
  * @since Apr 23, 2005
  */
-public class DefaultVisitor implements ValangVisitor {
+public class DefaultVisitor implements ValangVisitor, BeanFactoryAware, ApplicationContextAware, ResourceLoaderAware, MessageSourceAware, ApplicationEventPublisherAware, ServletContextAware {
 
 	private ValangVisitor visitor = null;
 	private DefaultDateParser dateParser = null;
+	
+	private BeanFactory beanFactory = null;
+	private ApplicationContext applicationContext = null;
+	private ResourceLoader resourceLoader = null;
+	private MessageSource messageSource = null;
+	private ApplicationEventPublisher applicationEventPublisher = null;
+	private ServletContext servletContext = null;
 	
 	public DefaultVisitor() {
 		super();
@@ -55,10 +80,14 @@ public class DefaultVisitor implements ValangVisitor {
 	}
 
 	public Function getFunction(String name, Function[] arguments, int line, int column) {
+		return lifeCycleCallbacks(doGetFunction(name, arguments, line, column), line, column);
+	}
+	
+	private Function doGetFunction(String name, Function[] arguments, int line, int column) {
 		if (getVisitor() != null) {
-			Function tmpFunction = getVisitor().getFunction(name, arguments, line, column);
-			if (tmpFunction != null) {
-				return tmpFunction;
+			Function function = getVisitor().getFunction(name, arguments, line, column);
+			if (function != null) {
+				return function;
 			}
 		}
 		
@@ -78,9 +107,59 @@ public class DefaultVisitor implements ValangVisitor {
 			return new ResolveFunction(arguments, line, column);
 		}
 
-		throw new IllegalArgumentException("Could not find function [" + name + "]!");
+		throw new ValangException("Could not find function [" + name + "]", line, column);
 	}
 
+	private Function lifeCycleCallbacks(Function function, int line, int column) {
+		if (function instanceof BeanFactoryAware) {
+			((BeanFactoryAware)function).setBeanFactory(beanFactory);
+		}
+		if (function instanceof ApplicationContextAware) {
+			((ApplicationContextAware)function).setApplicationContext(applicationContext);
+		}
+		if (function instanceof ResourceLoaderAware) {
+			((ResourceLoaderAware)function).setResourceLoader(resourceLoader);
+		}
+		if (function instanceof MessageSourceAware) {
+			((MessageSourceAware)function).setMessageSource(messageSource);
+		}
+		if (function instanceof ApplicationEventPublisherAware) {
+			((ApplicationEventPublisherAware)function).setApplicationEventPublisher(applicationEventPublisher);
+		}
+		if (function instanceof ServletContextAware) {
+			((ServletContextAware)function).setServletContext(servletContext);
+		}
+		if (function instanceof AbstractFunction) {
+			AbstractFunction abstractFunction = (AbstractFunction)function;
+			AutowireCapableBeanFactory autowireCapableBeanFactory = null;
+			
+			if (abstractFunction.isAutowireByName() || abstractFunction.isAutowireByType()) {
+				if (applicationContext instanceof ConfigurableApplicationContext) {
+					ConfigurableApplicationContext configurableApplicationContext = (ConfigurableApplicationContext)applicationContext;
+					autowireCapableBeanFactory = configurableApplicationContext.getBeanFactory();
+				} else if (beanFactory instanceof AutowireCapableBeanFactory) {
+					autowireCapableBeanFactory = (AutowireCapableBeanFactory)beanFactory;
+				} else if (applicationContext == null && beanFactory == null) {
+					throw new ValangException("Could not autowire function: no application context or bean factory available", line, column);
+				} else {
+					throw new ValangException("Could not autowire function: application context or bean factory does not support autowiring", line, column);
+				}
+			}
+			if (abstractFunction.isAutowireByName()) {
+				autowireCapableBeanFactory.autowireBeanProperties(function, AutowireCapableBeanFactory.AUTOWIRE_BY_NAME, false);
+			}
+			if (abstractFunction.isAutowireByType()) {
+				autowireCapableBeanFactory.autowireBeanProperties(function, AutowireCapableBeanFactory.AUTOWIRE_BY_TYPE, false);
+			}
+			try {
+				abstractFunction.init();
+			} catch (Exception e) {
+				throw new ValangException("Error initializing function", e, line, column);
+			}
+		}
+		return function;
+	}
+	
 	/**
 	 * <p>Register a custom visitor to look up custom functions. Lookup of functions
 	 * will first be delegated to this visitor. If no function has been returned (null)
@@ -105,5 +184,29 @@ public class DefaultVisitor implements ValangVisitor {
 			throw new IllegalStateException("Date parser not supported in Java 1.3 or older.");
 		}
 		return this.dateParser;
+	}
+
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		this.beanFactory = beanFactory;
+	}
+
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	public void setResourceLoader(ResourceLoader resourceLoader) {
+		this.resourceLoader = resourceLoader;
+	}
+
+	public void setMessageSource(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+
+	public void setServletContext(ServletContext servletContext) {
+		this.servletContext = servletContext;
+	}
+
+	public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+		this.applicationEventPublisher = applicationEventPublisher;
 	}
 }
