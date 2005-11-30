3bd7a25837d256f41cd679b162cfe59c3918d528
alruiz15 <alruiz15>
2005-11-30 01:04:06 +0000

    Added to CVS.

---
+++ projects/cache/src/test/org/springmodules/util/ReflectionsTest.java
@@ -0,0 +1,175 @@
+/* 
+ * Created on Nov 29, 2005
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright @2005 the original author or authors.
+ */
+package org.springmodules.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+/**
+ * <p>
+ * Unit Tests for <code>{@link Reflections}</code>.
+ * </p>
+ * 
+ * @author Alex Ruiz
+ */
+public class ReflectionsTest extends TestCase {
+
+  private static class TestBean {
+
+    private static String field4;
+
+    public static final String getField4() {
+      return field4;
+    }
+
+    public static final void setField4(String newField4) {
+      field4 = newField4;
+    }
+
+    private String field1;
+
+    private String field2;
+
+    private String field3;
+
+    private transient String field5;
+
+    public final String getField1() {
+      return field1;
+    }
+
+    public final String getField2() {
+      return field2;
+    }
+
+    public final String getField3() {
+      return field3;
+    }
+
+    public final String getField5() {
+      return field5;
+    }
+
+    public final void setField1(String newField1) {
+      field1 = newField1;
+    }
+
+    public final void setField2(String newField2) {
+      field2 = newField2;
+    }
+
+    public final void setField3(String newField3) {
+      field3 = newField3;
+    }
+
+    public final void setField5(String newField5) {
+      field5 = newField5;
+    }
+
+  }
+
+  public ReflectionsTest(String name) {
+    super(name);
+  }
+
+  public void testReflectionHashCodeWithCollection() {
+    int expected = 31 * 7 + "Han Solo".hashCode();
+    expected = 31 * expected + "Leia Organa".hashCode();
+
+    List list = new ArrayList();
+    list.add("Han Solo");
+    list.add("Leia Organa");
+    int actual = Reflections.reflectionHashCode(list);
+
+    assertEquals(expected, actual);
+  }
+
+  public void testReflectionHashCodeWithJavaBean() {
+    int expected = 31 * 7 + "field1".hashCode();
+    expected = 31 * expected + "field2".hashCode();
+    expected = 31 * expected + "field3".hashCode();
+
+    TestBean bean = new TestBean();
+    bean.setField1("field1");
+    bean.setField2("field2");
+    bean.setField3("field3");
+    TestBean.setField4("field4");
+    bean.setField5("field5");
+
+    int actual = Reflections.reflectionHashCode(bean);
+    assertEquals(expected, actual);
+  }
+
+  public void testReflectionHashCodeWithMap() {
+    int h1 = 31 * 7 + "Jedi".hashCode();
+    h1 = 31 * h1 + "Luke Skywalker".hashCode();
+
+    int h2 = 31 * 7 + "Sith".hashCode();
+    h2 = 31 * h2 + "Darth Vader".hashCode();
+
+    int expected = 31 * 7 + h1;
+    expected = 31 * expected + h2;
+
+    Map map = new HashMap();
+    map.put("Jedi", "Luke Skywalker");
+    map.put("Sith", "Darth Vader");
+    int actual = Reflections.reflectionHashCode(map);
+
+    assertEquals(expected, actual);
+  }
+
+  public void testReflectionHashCodeWithObjectArray() {
+    int expected = 31 * 7 + "Han Solo".hashCode();
+    expected = 31 * expected + "Leia Organa".hashCode();
+
+    Object[] array = { "Han Solo", "Leia Organa" };
+    int actual = Reflections.reflectionHashCode(array);
+
+    assertEquals(expected, actual);
+  }
+
+  public void testReflectionHashCodeWithObjectEqualToNull() {
+    assertEquals(0, Reflections.reflectionHashCode(null));
+  }
+
+  public void testReflectionHashCodeWithPrimitiveArray() {
+    boolean[] array = new boolean[] { true, false };
+    int expected = Objects.nullSafeHashCode(array);
+    int actual = Reflections.reflectionHashCode(array);
+
+    assertEquals(expected, actual);
+  }
+
+  public void testReflectionHashCodeWithPrimitiveWrapper() {
+    int expected = Boolean.TRUE.hashCode();
+    int actual = Reflections.reflectionHashCode(Boolean.TRUE);
+
+    assertEquals(expected, actual);
+  }
+
+  public void testReflectionHashCodeWithString() {
+    int expected = "X-Wing".hashCode();
+    int actual = Reflections.reflectionHashCode("X-Wing");
+
+    assertEquals(expected, actual);
+  }
+}
