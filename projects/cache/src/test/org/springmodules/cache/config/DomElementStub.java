224d210e9a67c0e0b17e26d553dc3c9368e3c39a
alruiz15 <alruiz15>
2006-01-29 20:55:30 +0000

    Issue number: MOD-77 Obtained from: Alex Ruiz Submitted by: Alex Ruiz Minor changes in Javadocs.

--- projects/cache/src/test/org/springmodules/cache/config/DomElementStub.java
+++ projects/cache/src/test/org/springmodules/cache/config/DomElementStub.java
@@ -53,50 +53,49 @@ public class DomElementStub implements Element {
   }
 
   /**
-   * @see org.w3c.dom.Node#appendChild(org.w3c.dom.Node)
+   * @see Node#appendChild(Node)
    */
   public Node appendChild(Node child) throws DOMException {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#cloneNode(boolean)
+   * @see Node#cloneNode(boolean)
    */
   public Node cloneNode(boolean deep) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#compareDocumentPosition(org.w3c.dom.Node)
+   * @see Node#compareDocumentPosition(Node)
    */
   public short compareDocumentPosition(Node other) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#getAttribute(java.lang.String)
+   * @see Element#getAttribute(String)
    */
   public String getAttribute(String name) {
     return (String) attributes.get(name);
   }
 
   /**
-   * @see org.w3c.dom.Element#getAttributeNode(java.lang.String)
+   * @see Element#getAttributeNode(String)
    */
   public Attr getAttributeNode(String name) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#getAttributeNodeNS(java.lang.String,
-   *      java.lang.String)
+   * @see Element#getAttributeNodeNS(String, String)
    */
   public Attr getAttributeNodeNS(String namespaceURI, String localName) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#getAttributeNS(java.lang.String, java.lang.String)
+   * @see Element#getAttributeNS(String, String)
    */
   public String getAttributeNS(String namespaceURI, String localName)
       throws DOMException {
@@ -104,304 +103,301 @@ public class DomElementStub implements Element {
   }
 
   /**
-   * @see org.w3c.dom.Node#getAttributes()
+   * @see Node#getAttributes()
    */
   public NamedNodeMap getAttributes() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getBaseURI()
+   * @see Node#getBaseURI()
    */
   public String getBaseURI() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getChildNodes()
+   * @see Node#getChildNodes()
    */
   public NodeList getChildNodes() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#getElementsByTagName(java.lang.String)
+   * @see Element#getElementsByTagName(String)
    */
   public NodeList getElementsByTagName(String name) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#getElementsByTagNameNS(java.lang.String,
-   *      java.lang.String)
+   * @see Element#getElementsByTagNameNS(String, String)
    */
   public NodeList getElementsByTagNameNS(String namespaceURI, String localName) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getFeature(java.lang.String, java.lang.String)
+   * @see Node#getFeature(String, String)
    */
   public Object getFeature(String feature, String version) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getFirstChild()
+   * @see Node#getFirstChild()
    */
   public Node getFirstChild() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getLastChild()
+   * @see Node#getLastChild()
    */
   public Node getLastChild() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getLocalName()
+   * @see Node#getLocalName()
    */
   public String getLocalName() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getNamespaceURI()
+   * @see Node#getNamespaceURI()
    */
   public String getNamespaceURI() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getNextSibling()
+   * @see Node#getNextSibling()
    */
   public Node getNextSibling() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getNodeName()
+   * @see Node#getNodeName()
    */
   public String getNodeName() {
     return nodeName;
   }
 
   /**
-   * @see org.w3c.dom.Node#getNodeType()
+   * @see Node#getNodeType()
    */
   public short getNodeType() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getNodeValue()
+   * @see Node#getNodeValue()
    */
   public String getNodeValue() throws DOMException {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getOwnerDocument()
+   * @see Node#getOwnerDocument()
    */
   public Document getOwnerDocument() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getParentNode()
+   * @see Node#getParentNode()
    */
   public Node getParentNode() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getPrefix()
+   * @see Node#getPrefix()
    */
   public String getPrefix() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getPreviousSibling()
+   * @see Node#getPreviousSibling()
    */
   public Node getPreviousSibling() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#getSchemaTypeInfo()
+   * @see Element#getSchemaTypeInfo()
    */
   public TypeInfo getSchemaTypeInfo() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#getTagName()
+   * @see Element#getTagName()
    */
   public String getTagName() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getTextContent()
+   * @see Node#getTextContent()
    */
   public String getTextContent() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#getUserData(java.lang.String)
+   * @see Node#getUserData(String)
    */
   public Object getUserData(String key) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#hasAttribute(java.lang.String)
+   * @see Element#hasAttribute(String)
    */
   public boolean hasAttribute(String newName) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#hasAttributeNS(java.lang.String, java.lang.String)
+   * @see Element#hasAttributeNS(String, String)
    */
   public boolean hasAttributeNS(String namespaceURI, String localName) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#hasAttributes()
+   * @see Node#hasAttributes()
    */
   public boolean hasAttributes() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#hasChildNodes()
+   * @see Node#hasChildNodes()
    */
   public boolean hasChildNodes() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#insertBefore(org.w3c.dom.Node, org.w3c.dom.Node)
+   * @see Node#insertBefore(Node, Node)
    */
   public Node insertBefore(Node child, Node refChild) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#isDefaultNamespace(java.lang.String)
+   * @see Node#isDefaultNamespace(String)
    */
   public boolean isDefaultNamespace(String namespaceURI) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#isEqualNode(org.w3c.dom.Node)
+   * @see Node#isEqualNode(Node)
    */
   public boolean isEqualNode(Node arg) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#isSameNode(org.w3c.dom.Node)
+   * @see Node#isSameNode(Node)
    */
   public boolean isSameNode(Node other) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#isSupported(java.lang.String, java.lang.String)
+   * @see Node#isSupported(String, String)
    */
   public boolean isSupported(String feature, String version) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#lookupNamespaceURI(java.lang.String)
+   * @see Node#lookupNamespaceURI(String)
    */
   public String lookupNamespaceURI(String prefix) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#lookupPrefix(java.lang.String)
+   * @see Node#lookupPrefix(String)
    */
   public String lookupPrefix(String namespaceURI) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#normalize()
+   * @see Node#normalize()
    */
   public void normalize() {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#removeAttribute(java.lang.String)
+   * @see Element#removeAttribute(String)
    */
   public void removeAttribute(String name) {
     attributes.remove(name);
   }
 
   /**
-   * @see org.w3c.dom.Element#removeAttributeNode(org.w3c.dom.Attr)
+   * @see Element#removeAttributeNode(Attr)
    */
   public Attr removeAttributeNode(Attr oldAttr) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#removeAttributeNS(java.lang.String,
-   *      java.lang.String)
+   * @see Element#removeAttributeNS(String, String)
    */
   public void removeAttributeNS(String namespaceURI, String localName) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#removeChild(org.w3c.dom.Node)
+   * @see Node#removeChild(Node)
    */
   public Node removeChild(Node oldChild) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#replaceChild(org.w3c.dom.Node, org.w3c.dom.Node)
+   * @see Node#replaceChild(Node, Node)
    */
   public Node replaceChild(Node child, Node oldChild) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#setAttribute(java.lang.String, java.lang.String)
+   * @see Element#setAttribute(String, String)
    */
   public void setAttribute(String name, String value) {
     attributes.put(name, value);
   }
 
   /**
-   * @see org.w3c.dom.Element#setAttributeNode(org.w3c.dom.Attr)
+   * @see Element#setAttributeNode(Attr)
    */
   public Attr setAttributeNode(Attr attr) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#setAttributeNodeNS(org.w3c.dom.Attr)
+   * @see Element#setAttributeNodeNS(Attr)
    */
   public Attr setAttributeNodeNS(Attr attr) throws DOMException {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#setAttributeNS(java.lang.String, java.lang.String,
-   *      java.lang.String)
+   * @see Element#setAttributeNS(String, String, String)
    */
   public void setAttributeNS(String namespaceURI, String qualifiedName,
       String newValue) {
@@ -409,22 +405,21 @@ public class DomElementStub implements Element {
   }
 
   /**
-   * @see org.w3c.dom.Element#setIdAttribute(java.lang.String, boolean)
+   * @see Element#setIdAttribute(String, boolean)
    */
   public void setIdAttribute(String name, boolean isId) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#setIdAttributeNode(org.w3c.dom.Attr, boolean)
+   * @see Element#setIdAttributeNode(Attr, boolean)
    */
   public void setIdAttributeNode(Attr idAttr, boolean isId) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Element#setIdAttributeNS(java.lang.String,
-   *      java.lang.String, boolean)
+   * @see Element#setIdAttributeNS(String, String, boolean)
    */
   public void setIdAttributeNS(String namespaceURI, String localName,
       boolean newIsId) {
@@ -432,29 +427,28 @@ public class DomElementStub implements Element {
   }
 
   /**
-   * @see org.w3c.dom.Node#setNodeValue(java.lang.String)
+   * @see Node#setNodeValue(String)
    */
   public void setNodeValue(String nodeValue) throws DOMException {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#setPrefix(java.lang.String)
+   * @see Node#setPrefix(String)
    */
   public void setPrefix(String prefix) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#setTextContent(java.lang.String)
+   * @see Node#setTextContent(String)
    */
   public void setTextContent(String textContent) {
     throw new UnsupportedOperationException();
   }
 
   /**
-   * @see org.w3c.dom.Node#setUserData(java.lang.String, java.lang.Object,
-   *      org.w3c.dom.UserDataHandler)
+   * @see Node#setUserData(String, Object, UserDataHandler)
    */
   public Object setUserData(String key, Object data, UserDataHandler handler) {
     throw new UnsupportedOperationException();
