f872eafdd41d7ea181575179b0d5ff7d11f0e0a0
alruiz15 <alruiz15>
2006-03-09 03:49:53 +0000

    Issue number:  MOD-77 Added to CVS.

---
+++ projects/cache/tiger/test/org/springmodules/cache/config/ehcache/EhCacheAnnotationsParserTests.java
@@ -0,0 +1,63 @@
+/* 
+ * Created on Mar 8, 2006
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * Copyright @2006 the original author or authors.
+ */
+package org.springmodules.cache.config.ehcache;
+
+import org.springmodules.cache.config.CacheModelParser;
+import org.springmodules.cache.config.CacheProviderFacadeDefinitionValidator;
+
+import junit.framework.TestCase;
+
+/**
+ * <p>
+ * Unit Tests for <code>{@link EhCacheAnnotationsParser}</code>.
+ * </p>
+ * 
+ * @author Alex Ruiz
+ */
+public class EhCacheAnnotationsParserTests extends TestCase {
+
+  private EhCacheAnnotationsParser parser;
+
+  /**
+   * Constructor.
+   * 
+   * @param name
+   *          the name of the test case
+   */
+  public EhCacheAnnotationsParserTests(String name) {
+    super(name);
+  }
+
+  public void testGetCacheModelParser() {
+    CacheModelParser cacheModelParser = parser.getCacheModelParser();
+    EhCacheConfigAssert.assertCacheModelParserIsCorrect(cacheModelParser);
+  }
+
+  public void testGetCacheProviderFacadeDefinitionValidator() {
+    CacheProviderFacadeDefinitionValidator validator = parser
+        .getCacheProviderFacadeDefinitionValidator();
+    EhCacheConfigAssert
+        .assertCacheProviderFacadeDefinitionValidatorIsCorrect(validator);
+  }
+
+  @Override
+  protected void setUp() {
+    parser = new EhCacheAnnotationsParser();
+  }
+
+}
