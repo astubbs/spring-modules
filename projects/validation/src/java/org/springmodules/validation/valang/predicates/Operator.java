45f486aa7efb010bdbb47a072e6c31d4384219f8
hueboness <hueboness>
2006-05-27 02:22:37 +0000

    Issue number:  MOD-131 - Moved the validation module out of the sandbox to the projects Submitted by:  Uri Boness

---
+++ projects/validation/src/java/org/springmodules/validation/valang/predicates/Operator.java
@@ -0,0 +1,65 @@
+package org.springmodules.validation.valang.predicates;
+
+/**
+ * <p>The Operator interface represents an operator in a validation rule.
+ * 
+ * @author Steven Devijver
+ * @since 23-04-2005
+ */
+public interface Operator {
+	public interface EqualsOperator extends Operator {}
+	
+	public interface NotEqualsOperator extends Operator {}
+	
+	public interface LessThanOperator extends Operator {}
+	
+	public interface LessThanOrEqualOperator extends Operator {}
+	
+	public interface MoreThanOperator extends Operator {}
+	
+	public interface MoreThanOrEqualOperator extends Operator {}
+	
+	public interface InOperator extends Operator {}
+	
+	public interface NotInOperator extends Operator {}
+	
+	public interface BetweenOperator extends Operator {}
+	
+	public interface NotBetweenOperator extends Operator {}
+	
+	public interface NullOperator extends Operator {}
+	
+	public interface NotNullOperator extends Operator {}
+
+	public interface HasTextOperator extends Operator {}
+	
+	public interface HasNoTextOperator extends Operator {}
+	
+	public interface HasLengthOperator extends Operator {}
+	
+	public interface HasNoLengthOperator extends Operator {}
+
+	
+	public interface IsBlankOperator extends Operator {}
+	
+	
+	public interface IsNotBlankOperator extends Operator {}
+	
+	
+	public interface IsWordOperator extends Operator {}
+	
+	
+	public interface IsNotWordOperator extends Operator {}
+	
+	
+	public interface IsUpperCaseOperator extends Operator {}
+	
+	
+	public interface IsNotUpperCaseOperator extends Operator {}
+	
+	
+	public interface IsLowerCaseOperator extends Operator {}
+	
+	
+	public interface IsNotLowerCaseOperator extends Operator {}
+}
---
+++ projects/validation/src/test/org/springmodules/validation/valang/parser/Person.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 13/09/2005 (C) Our Community Pty. Ltd. All Rights Reserved.
+ *
+ * $Id: Person.java,v 1.1 2006/05/27 02:23:10 hueboness Exp $
+ */
+package org.springmodules.validation.valang.parser;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Person {
+
+    int age = 0;
+    String firstName = null;
+    String size = null;
+    Date dateOfBirth = null;
+
+    int minAge = 18;
+
+    public Person(int age, String firstName) {
+        setAge(age);
+        setFirstName(firstName);
+    }
+
+    public Person(String size) {
+        setSize(size);
+    }
+
+    public Person(Date dateOfBirth) {
+        setDateOfBirth(dateOfBirth);
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSize() {
+        return this.size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public Date getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getMinAge() {
+        return this.minAge;
+    }
+
+    public Object[] getSizes() {
+        return new Object[] { "S", "M", "L", "XL" };
+    }
+
+    public Collection getTags() {
+        Collection tags = new ArrayList();
+        tags.add("tag1");
+        tags.add("tag2");
+        return tags;
+    }
+
+    public Map getMap() {
+        Map map = new HashMap();
+        map.put("firstName", "Steven"); map.put("sizes", getSizes());
+        return map;
+    }
+
+}
