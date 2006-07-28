0a2fbb8f05ad3faece025744b10a1e377d4a5478
hueboness <hueboness>
2006-07-28 01:29:10 +0000

    - removed the ognl element definition from the validation.xsd - added javadoc to cel and fel packages - renamed BeanWrapperFunctionExpressionParser to PropertyPathFunctoinExpressionParser

--- sandbox/validation/src/java/org/springmodules/validation/util/cel/parser/OgnlConditionExpressionParser.java
+++ sandbox/validation/src/java/org/springmodules/validation/util/cel/parser/OgnlConditionExpressionParser.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2004-2005 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springmodules.validation.util.cel.parser;
 
 import org.springmodules.validation.util.cel.ConditionExpressionParser;
@@ -6,6 +22,9 @@ import org.springmodules.validation.util.condition.Condition;
 import org.springmodules.validation.util.condition.parser.ognl.OgnlCondition;
 
 /**
+ * A {@link ConditionExpressionParser} implementation that knows how to parse boolean OGNL expressions and return
+ * the appropriate condition.
+ *
  * @author Uri Boness
  */
 public class OgnlConditionExpressionParser implements ConditionExpressionParser {
