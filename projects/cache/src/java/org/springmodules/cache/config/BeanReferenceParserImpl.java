1bc1bcbc20159813d5a315d36f209b0b88b8baca
irbouho <irbouho>
2006-10-15 08:22:46 +0000

    Spring 2.0 API changes

--- projects/cache/src/java/org/springmodules/cache/config/BeanReferenceParserImpl.java
+++ projects/cache/src/java/org/springmodules/cache/config/BeanReferenceParserImpl.java
@@ -1,12 +1,12 @@
-/* 
+/*
  * Created on Mar 13, 2006
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -34,7 +34,8 @@ import org.springframework.util.xml.DomUtils;
  * <p>
  * Default implementation of <code>{@link BeanReferenceParser}</code>.
  * </p>
- * 
+ *
+ * @author Omar Irbouh
  * @author Alex Ruiz
  */
 public class BeanReferenceParserImpl implements BeanReferenceParser {
@@ -69,8 +70,9 @@ public class BeanReferenceParserImpl implements BeanReferenceParser {
           + "bean definition");
     }
 
-    BeanDefinitionHolder holder = parserContext.getDelegate()
-        .parseBeanDefinitionElement(beanElement, true);
+		BeanDefinitionHolder holder = parserContext.getDelegate()
+        .parseBeanDefinitionElement(beanElement,
+						parserContext.getContainingBeanDefinition());
 
     String beanName = holder.getBeanName();
 
