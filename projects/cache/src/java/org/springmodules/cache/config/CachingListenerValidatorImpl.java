6fb93c1127fd52d120ea6990028b07ed2e0c0f72
irbouho <irbouho>
2006-10-15 10:29:38 +0000

    resolve beanDefinition class prior to validating.

--- projects/cache/src/java/org/springmodules/cache/config/CachingListenerValidatorImpl.java
+++ projects/cache/src/java/org/springmodules/cache/config/CachingListenerValidatorImpl.java
@@ -1,12 +1,12 @@
-/* 
+/*
  * Created on Mar 31, 2006
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -23,6 +23,7 @@ import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.util.ClassUtils;
 
 import org.springmodules.cache.interceptor.caching.CachingListener;
 
@@ -30,7 +31,8 @@ import org.springmodules.cache.interceptor.caching.CachingListener;
  * <p>
  * Default implementation of <code>{@link CachingListenerValidator}</code>.
  * </p>
- * 
+ *
+ * @author Omar Irbouh
  * @author Alex Ruiz
  */
 public class CachingListenerValidatorImpl implements CachingListenerValidator {
@@ -58,12 +60,32 @@ public class CachingListenerValidatorImpl implements CachingListenerValidator {
     }
 
     Class expectedClass = CachingListener.class;
-    Class actualClass = ((AbstractBeanDefinition) beanDefinition)
-        .getBeanClass();
+    Class actualClass = resolveBeanClass(beanDefinition);
 
     if (!expectedClass.isAssignableFrom(actualClass)) {
       throw new IllegalStateException("The caching listener [" + index
           + "] should be an instance of <" + expectedClass.getName() + ">");
     }
   }
+
+	/**
+	 * Resolves class of a beanDefinition
+	 * @param beanDefinition the bean definition
+	 * @return class of the bean definition
+	 * @throws IllegalStateException if the given bean definition class can not
+	 * be resolved
+	 * @see org.springframework.beans.factory.config.BeanDefinition#setBeanClassName(String)
+	 * @see org.springframework.beans.factory.support.AbstractBeanDefinition#resolveBeanClass(ClassLoader)
+	 */
+	protected Class resolveBeanClass(BeanDefinition beanDefinition)
+			throws IllegalStateException {
+		try {
+			return ((AbstractBeanDefinition) beanDefinition)
+					.resolveBeanClass(ClassUtils.getDefaultClassLoader());
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Could not resolve class ["
+					+ beanDefinition.getBeanClassName() + "] of caching listener");
+		}
+	}
+
 }
