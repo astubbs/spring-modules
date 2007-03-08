9657bb545515c55eac58e69af67dc7227779c048
sbtourist <sbtourist>
2007-03-08 18:13:55 +0000

    Minor fixes in docs and javadocs.

--- projects/xt/src/java/org/springmodules/xt/ajax/action/AbstractRenderingAction.java
+++ projects/xt/src/java/org/springmodules/xt/ajax/action/AbstractRenderingAction.java
@@ -22,16 +22,23 @@ import org.springmodules.xt.ajax.AjaxAction;
 import org.springmodules.xt.ajax.component.Component;
 
 /**
- * Taconite based action which refers a particular html element (identified by its id) and renders components acting on the referred element.<br>
- * The concrete rendering type (element replacing, content replacing etc.) is defined by subclasses via template methods.
+ * Taconite based action which matches html elements by their identifiers (id), 
+ * and renders components acting on these elements.<br>
+ * The action is constructed by passing a target identifier, that is the one of the target HTML element to match, and
+ * a list of components to render.<br>
+ * By default, the action matches the element whose identifier is equal to the target one.<br>
+ * If the multiple matching capability is enabled (see {@link #setMultipleMatch(boolean)}),
+ * the action will match elements whose identifier is exactly equal to the target one, or 
+ * starts with the same substring and ends with the "_" wildcard character.<br><br>
+ * The concrete action (element replacing, content replacing etc.) is defined by subclasses via template methods.
  *
  * @author Sergio Bossa
  */
 public abstract class AbstractRenderingAction implements AjaxAction {
     
     private String elementId;
-    private List<Component> components = new LinkedList();
     private boolean multipleMatch;
+    private List<Component> components = new LinkedList();
     
     /**
      * Construct the action.
@@ -52,16 +59,19 @@ public abstract class AbstractRenderingAction implements AjaxAction {
         this.elementId = elementId;
         this.components.add(component);
     }
-     /**
-      * If true, this action will match multiple elements with id matching the id 
-      * passed to this action.<br>
-      * An HTML element id matches if it is equals to this id, or if it contains
-      * the '*' wildcard.
-      */
+    
+    /**
+     * Make this action matching multiple elements whose identifier is exactly equal to the
+     * identifier passed to this action, or starts with the same substring and ends with 
+     * the "_" wildcard.
+    */
     public void setMultipleMatch(boolean multipleMatch) {
         this.multipleMatch = multipleMatch;
     }
     
+    /**
+     * True if multiple matching is enabled, false otherwise.
+     */
     public boolean isMultipleMatch() {
         return this.multipleMatch;
     }
@@ -80,7 +90,18 @@ public abstract class AbstractRenderingAction implements AjaxAction {
         return response.toString();
     }
     
+    /**
+     * Get the action opening tag.<br>
+     * It must contain the following placeholders:
+     * <ul>
+     * <li>The "$1" placeholder, for putting the element id to match.</li>
+     * <li>The "$2" placeholder, for enabling the multiple matching capability.</li>
+     * </ul>
+     */
     protected abstract String getOpeningTag();
     
+    /**
+     * Get the action closing tag.
+     */
     protected abstract String getClosingTag();
 }
