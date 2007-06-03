202c3dd1430c75e169fa92b279d4315265241050
irbouho <irbouho>
2007-06-03 21:13:02 +0000

    fixed failing test

--- projects/cache/src/test/org/springmodules/cache/config/CacheProxyFactoryBeanParserTests.java
+++ projects/cache/src/test/org/springmodules/cache/config/CacheProxyFactoryBeanParserTests.java
@@ -1,12 +1,12 @@
-/* 
+/*
  * Created on Mar 2, 2006
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -29,7 +29,7 @@ import org.springframework.beans.factory.support.RootBeanDefinition;
  * <p>
  * Unit Tests for <code>{@link CacheProxyFactoryBeanParser}</code>.
  * </p>
- * 
+ *
  * @author Alex Ruiz
  */
 public class CacheProxyFactoryBeanParserTests extends
@@ -54,7 +54,7 @@ public class CacheProxyFactoryBeanParserTests extends
 
   /**
    * Constructor.
-   * 
+   *
    * @param name
    *          the name of the test case
    */
@@ -66,7 +66,7 @@ public class CacheProxyFactoryBeanParserTests extends
     Element proxyElement = proxyElementBuilder.toXml();
 
     RootBeanDefinition beanDefinition = new RootBeanDefinition(String.class);
-    BeanDefinitionHolder holder = new BeanDefinitionHolder(beanDefinition, null);
+    BeanDefinitionHolder holder = new BeanDefinitionHolder(beanDefinition, "beanName");
     beanReferenceParser.parse(proxyElement, parserContext);
     beanReferenceParserControl.setReturnValue(holder);
     beanReferenceParserControl.replay();
