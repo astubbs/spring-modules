ca6cc4947b10685f2c170974c850a44fb480dcca
irbouho <irbouho>
2007-06-13 02:37:30 +0000

    AbstractCachingInterceptor no longer logs exceptions throwed by advised methods as errors, log a debug message instead. MOD-289

--- projects/cache/src/java/org/springmodules/cache/interceptor/caching/AbstractCachingInterceptor.java
+++ projects/cache/src/java/org/springmodules/cache/interceptor/caching/AbstractCachingInterceptor.java
@@ -13,10 +13,24 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  *
- * Copyright @2004 the original author or authors.
+ * Copyright @2007 the original author or authors.
  */
-
 package org.springmodules.cache.interceptor.caching;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+import org.springmodules.cache.CachingModel;
+import org.springmodules.cache.FatalCacheException;
+import org.springmodules.cache.key.CacheKeyGenerator;
+import org.springmodules.cache.key.HashCodeCacheKeyGenerator;
+import org.springmodules.cache.provider.CacheModelValidator;
+import org.springmodules.cache.provider.CacheProviderFacade;
 
 import java.beans.PropertyEditor;
 import java.io.Serializable;
@@ -26,190 +40,174 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Properties;
 
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.util.CollectionUtils;
-import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
-
-import org.springmodules.cache.CachingModel;
-import org.springmodules.cache.FatalCacheException;
-import org.springmodules.cache.key.CacheKeyGenerator;
-import org.springmodules.cache.key.HashCodeCacheKeyGenerator;
-import org.springmodules.cache.provider.CacheModelValidator;
-import org.springmodules.cache.provider.CacheProviderFacade;
-
 /**
- * <p>
  * Template for advices that store in a cache the return value of intercepted
  * methods.
- * </p>
  *
+ * @author Omar Irbouh
  * @author Alex Ruiz
  */
 public abstract class AbstractCachingInterceptor implements MethodInterceptor,
-    InitializingBean {
+		InitializingBean {
 
-  public static final NullObject NULL_ENTRY = new NullObject();
+	protected final Log logger = LogFactory.getLog(getClass());
 
-  protected final Log logger = LogFactory.getLog(getClass());
+	public static final NullObject NULL_ENTRY = new NullObject();
 
-  private CacheProviderFacade cache;
+	private CacheProviderFacade cache;
 
-  private CacheKeyGenerator keyGenerator;
+	private CacheKeyGenerator keyGenerator;
 
-  private CachingListener[] listeners;
+	private CachingListener[] listeners;
 
-  private Map modelMap;
+	private Map modelMap;
 
-  public final void afterPropertiesSet() throws FatalCacheException {
-    validateCache();
-    if (modelMap instanceof Properties) setCachingModels(propertiesToModels());
-    validateModels();
-    if (keyGenerator == null) setCacheKeyGenerator(defaultKeyGenerator());
-    onAfterPropertiesSet();
-  }
+	public final void afterPropertiesSet() throws FatalCacheException {
+		validateCache();
+		if (modelMap instanceof Properties)
+			setCachingModels(propertiesToModels());
+		validateModels();
+		if (keyGenerator == null) setCacheKeyGenerator(defaultKeyGenerator());
+		onAfterPropertiesSet();
+	}
 
-  public final CacheKeyGenerator cacheKeyGenerator() {
-    return keyGenerator;
-  }
+	public final CacheKeyGenerator cacheKeyGenerator() {
+		return keyGenerator;
+	}
 
-  public final Object invoke(MethodInvocation mi) throws Throwable {
-    Method method = mi.getMethod();
-    if (!CachingUtils.isCacheable(method))
-      return methodNotCacheable(mi, method);
+	public final Object invoke(MethodInvocation mi) throws Throwable {
+		Method method = mi.getMethod();
+		if (!CachingUtils.isCacheable(method))
+			return methodNotCacheable(mi, method);
 
-    CachingModel model = model(mi);
-    if (model == null) return noModelFound(mi, method);
+		CachingModel model = model(mi);
+		if (model == null) return noModelFound(mi, method);
 
-    Serializable key = keyGenerator.generateKey(mi);
-    Object cached = cache.getFromCache(key, model);
+		Serializable key = keyGenerator.generateKey(mi);
+		Object cached = cache.getFromCache(key, model);
 
-    if (null == cached) return cachedValueFromSource(mi, key, model);
-    return unmaskNull(cached);
-  }
+		if (null == cached) return cachedValueFromSource(mi, key, model);
+		return unmaskNull(cached);
+	}
 
-  public final void setCacheKeyGenerator(CacheKeyGenerator k) {
-    keyGenerator = k;
-  }
+	public final void setCacheKeyGenerator(CacheKeyGenerator k) {
+		keyGenerator = k;
+	}
 
-  public final void setCacheProviderFacade(CacheProviderFacade c) {
-    cache = c;
-  }
+	public final void setCacheProviderFacade(CacheProviderFacade c) {
+		cache = c;
+	}
 
-  public final void setCachingListeners(CachingListener[] l) {
-    listeners = l;
-  }
+	public final void setCachingListeners(CachingListener[] l) {
+		listeners = l;
+	}
 
-  public final void setCachingModels(Map m) {
-    modelMap = m;
-  }
+	public final void setCachingModels(Map m) {
+		modelMap = m;
+	}
 
-  protected abstract CachingModel model(MethodInvocation mi);
+	protected abstract CachingModel model(MethodInvocation mi);
 
-  protected final Map models() {
-    return modelMap;
-  }
+	protected final Map models() {
+		return modelMap;
+	}
 
-  protected void onAfterPropertiesSet() throws FatalCacheException {
-    // no implementation.
-  }
+	protected void onAfterPropertiesSet() throws FatalCacheException {
+		// no implementation.
+	}
 
-  private Object cachedValueFromSource(MethodInvocation mi, Serializable key,
-      CachingModel m) throws Throwable {
-    boolean successful = true;
-    try {
-      Object value = mi.proceed();
-      putInCache(key, m, value);
-      return value;
-    } catch (Throwable t) {
-      successful = false;
-      logger.error("Unable to proceed to the next interceptor in the chain", t);
-      throw t;
-    } finally {
-      if (!successful) cache.cancelCacheUpdate(key);
-    }
-  }
+	private Object cachedValueFromSource(MethodInvocation mi, Serializable key,
+										 CachingModel m) throws Throwable {
+		boolean successful = true;
+		try {
+			Object value = mi.proceed();
+			putInCache(key, m, value);
+			return value;
+		} catch (Throwable t) {
+			successful = false;
+			logger.debug("method " + mi.getMethod().getName() + " throwed a exception", t);
+			throw t;
+		} finally {
+			if (!successful) cache.cancelCacheUpdate(key);
+		}
+	}
 
-  private CacheKeyGenerator defaultKeyGenerator() {
-    return new HashCodeCacheKeyGenerator(true);
-  }
+	private CacheKeyGenerator defaultKeyGenerator() {
+		return new HashCodeCacheKeyGenerator(true);
+	}
 
-  private Object logAndProceed(String message, MethodInvocation mi)
-      throws Throwable {
-    logger.debug(message);
-    return mi.proceed();
-  }
+	private Object logAndProceed(String message, MethodInvocation mi)
+			throws Throwable {
+		logger.debug(message);
+		return mi.proceed();
+	}
 
-  private Object maskNull(Object o) {
-    return o != null ? o : NULL_ENTRY;
-  }
+	private Object maskNull(Object o) {
+		return o != null ? o : NULL_ENTRY;
+	}
 
-  private Object methodNotCacheable(MethodInvocation mi, Method m)
-      throws Throwable {
-    return logAndProceed("Unable to perform caching. Intercepted method <"
-        + m + "> does not return a value", mi);
-  }
+	private Object methodNotCacheable(MethodInvocation mi, Method m)
+			throws Throwable {
+		return logAndProceed("Unable to perform caching. Intercepted method <"
+				+ m + "> does not return a value", mi);
+	}
 
-  private Object noModelFound(MethodInvocation mi, Method m) throws Throwable {
-    return logAndProceed("Unable to perform caching. "
-        + "No model is associated to the method <" + m + ">", mi);
-  }
+	private Object noModelFound(MethodInvocation mi, Method m) throws Throwable {
+		return logAndProceed("Unable to perform caching. "
+				+ "No model is associated to the method <" + m + ">", mi);
+	}
 
-  private void notifyListeners(Serializable key, Object cachedObject,
-      CachingModel m) {
-    if (ObjectUtils.isEmpty(listeners)) return;
-    for (int i = 0; i < listeners.length; i++)
-      listeners[i].onCaching(key, cachedObject, m);
-  }
+	private void notifyListeners(Serializable key, Object cachedObject,
+								 CachingModel m) {
+		if (ObjectUtils.isEmpty(listeners)) return;
+		for (int i = 0; i < listeners.length; i++)
+			listeners[i].onCaching(key, cachedObject, m);
+	}
 
-  private Map propertiesToModels() {
-    PropertyEditor editor = cache.getCachingModelEditor();
-    Properties properties = (Properties)modelMap;
+	private Map propertiesToModels() {
+		PropertyEditor editor = cache.getCachingModelEditor();
+		Properties properties = (Properties) modelMap;
 
-    Map m = new HashMap();
-    for (Iterator i = properties.keySet().iterator(); i.hasNext();) {
-      String id = (String)i.next();
-      editor.setAsText(properties.getProperty(id));
-      m.put(id, editor.getValue());
-    }
-    return m;
-  }
+		Map m = new HashMap();
+		for (Iterator i = properties.keySet().iterator(); i.hasNext();) {
+			String id = (String) i.next();
+			editor.setAsText(properties.getProperty(id));
+			m.put(id, editor.getValue());
+		}
+		return m;
+	}
 
-  private void putInCache(Serializable key, CachingModel m, Object o) {
-    cache.putInCache(key, m, maskNull(o));
-    notifyListeners(key, o, m);
-  }
+	private void putInCache(Serializable key, CachingModel m, Object o) {
+		cache.putInCache(key, m, maskNull(o));
+		notifyListeners(key, o, m);
+	}
 
-  private Object unmaskNull(Object obj) {
-    return NULL_ENTRY.equals(obj) ? null : obj;
-  }
+	private Object unmaskNull(Object obj) {
+		return NULL_ENTRY.equals(obj) ? null : obj;
+	}
 
-  private void validateCache() throws FatalCacheException {
-    if (cache == null)
-      throw new FatalCacheException(
-          "The cache provider facade should not be null");
-  }
+	private void validateCache() throws FatalCacheException {
+		if (cache == null)
+			throw new FatalCacheException(
+					"The cache provider facade should not be null");
+	}
 
-  private void validateModels() throws FatalCacheException {
-    if (CollectionUtils.isEmpty(modelMap))
-      throw new FatalCacheException(
-          "The map of caching models should not be empty");
+	private void validateModels() throws FatalCacheException {
+		if (CollectionUtils.isEmpty(modelMap))
+			throw new FatalCacheException(
+					"The map of caching models should not be empty");
 
-    CacheModelValidator validator = cache.modelValidator();
-    String id = null;
-    try {
-      for (Iterator i = modelMap.keySet().iterator(); i.hasNext();) {
-        id = (String)i.next();
-        validator.validateCachingModel(modelMap.get(id));
-      }
-    } catch (Exception exception) {
-      throw new FatalCacheException("Unable to validate caching model with id "
-          + StringUtils.quote(id), exception);
-    }
-  }
+		CacheModelValidator validator = cache.modelValidator();
+		String id = null;
+		try {
+			for (Iterator i = modelMap.keySet().iterator(); i.hasNext();) {
+				id = (String) i.next();
+				validator.validateCachingModel(modelMap.get(id));
+			}
+		} catch (Exception exception) {
+			throw new FatalCacheException("Unable to validate caching model with id "
+					+ StringUtils.quote(id), exception);
+		}
+	}
+
 }
