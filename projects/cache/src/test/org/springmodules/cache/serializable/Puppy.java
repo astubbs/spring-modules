f6fbb5d257950b39c24c8870dc23326a32e59122
irbouho <irbouho>
2007-07-05 02:18:58 +0000

    polishing...

--- projects/cache/src/test/org/springmodules/cache/serializable/Puppy.java
+++ projects/cache/src/test/org/springmodules/cache/serializable/Puppy.java
@@ -1,19 +1,19 @@
-/* 
+/*
  * Created on Aug 11, 2005
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
  *
- * Copyright @2005 the original author or authors.
+ * Copyright @2007 the original author or authors.
  */
 package org.springmodules.cache.serializable;
 
@@ -23,72 +23,62 @@ import org.springframework.util.StringUtils;
 import org.springmodules.util.Objects;
 
 /**
- * <p>
  * Abstraction of a pet.
- * </p>
- * 
+ *
+ * @author Omar Irbouh
  * @author Alex Ruiz
  */
 public class Puppy {
 
-  private String name;
+	private String name;
 
-  public Puppy() {
-    super();
-  }
+	public Puppy() {
+	}
 
-  public Puppy(String name) {
-    super();
-    setName(name);
-  }
+	public Puppy(String name) {
+		setName(name);
+	}
 
-  /**
-   * @see Object#equals(Object)
-   */
-  public boolean equals(Object obj) {
-    if (obj == null) {
-      return false;
-    }
+	/**
+	 * @see Object#equals(Object)
+	 */
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
 
-    if (!(obj instanceof Puppy)) {
-      return false;
-    }
+		if (!(obj instanceof Puppy)) {
+			return false;
+		}
 
-    Puppy puppy = (Puppy) obj;
-    if (!ObjectUtils.nullSafeEquals(name, puppy.name)) {
-      return false;
-    }
+		Puppy puppy = (Puppy) obj;
+		return ObjectUtils.nullSafeEquals(name, puppy.name);
+	}
 
-    return true;
-  }
+	public final String getName() {
+		return name;
+	}
 
-  public final String getName() {
-    return name;
-  }
+	/**
+	 * @see Object#hashCode()
+	 */
+	public int hashCode() {
+		return Objects.nullSafeHashCode(name);
+	}
 
-  /**
-   * @see Object#hashCode()
-   */
-  public int hashCode() {
-    int multiplier = 31;
-    int hash = 17;
+	public final void setName(String name) {
+		this.name = name;
+	}
 
-    hash = multiplier * hash + Objects.nullSafeHashCode(name);
-
-    return hash;
-  }
-
-  public final void setName(String newName) {
-    name = newName;
-  }
-
-  /**
-   * @see Object#toString()
-   */
-  public String toString() {
-    StringBuffer buffer = Objects.identityToString(this);
-    buffer.append("[name=" + StringUtils.quote(name) + "]");
-    return buffer.toString();
-  }
+	/**
+	 * @see Object#toString()
+	 */
+	public String toString() {
+		return Objects.identityToString(this)
+				.append("[name=")
+				.append(StringUtils.quote(name))
+				.append(']')
+				.toString();
+	}
 
 }
