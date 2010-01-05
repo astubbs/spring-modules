6dc9365bd12f30404e0f3971c9ff23ab265a840f
David J. M. Karlsen <david@davidkarlsen.com>
2010-01-05 03:38:18 +0100

    docs

--- projects/spring-modules-cache/src/main/java/org/springmodules/cache/key/MultiKeyCodeGenerator.java
+++ projects/spring-modules-cache/src/main/java/org/springmodules/cache/key/MultiKeyCodeGenerator.java
@@ -5,22 +5,40 @@ import java.io.Serializable;
 import org.aopalliance.intercept.MethodInvocation;
 import org.apache.commons.collections.keyvalue.MultiKey;
 
+/**
+ * 
+ * A {@link CacheKeyGenerator} based on commons collections.
+ * 
+ * @author David J. M. Karlsen <david@davidkarlsen.com>
+ *
+ */
 public class MultiKeyCodeGenerator
     implements CacheKeyGenerator
 {
     private final boolean cloneKeys;
     
+    /**
+     * @param cloneKeys set to true to clone keys to assure they stick immutable.
+     */
     public MultiKeyCodeGenerator( boolean cloneKeys )
     {
         this.cloneKeys = cloneKeys;
     }
     
+    /**
+     * Equals <code>this(true)</code>
+     */
     public MultiKeyCodeGenerator()
     {
         this( true );
     }
     
-    public Serializable generateKey( MethodInvocation methodInvocation )
+    /**
+     * Generates a key based on {@linkplain MultiKey}.
+     * {@inheritDoc}
+     * @return a {@link MultiKey} co-variant of {@link Serializable}.
+     */
+    public MultiKey generateKey( MethodInvocation methodInvocation )
     {
         Object[] arguments = methodInvocation.getArguments();
         MultiKey multiKey = new MultiKey( arguments, cloneKeys );
